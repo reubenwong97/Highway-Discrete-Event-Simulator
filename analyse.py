e2ef7ed804e24ccce13f7d5e7c55e0c5d9b89c00
Reuben Wong <reuben.wong.ys@gmail.com>
2021-04-06 00:18:14 +0800

    [FULLY AUTOMATE] added confidence interval to analyse.py

--- analyse.py
+++ analyse.py
@@ -1,5 +1,7 @@
 import numpy as np
 import argparse
+from scipy import stats
+import scipy
 
 def analyse(args):
     dropped_name = f'./results/reserve_{args.num_reserve}/reps_{args.reps}_dropped.npy'
@@ -8,6 +10,9 @@ def analyse(args):
     
     dropped_arr = np.load(dropped_name)
     blocked_arr = np.load(blocked_name)
+
+    dof = len(dropped_arr) - 1
+    t_value = stats.t.ppf(1-args.alpha/2, dof)
 
     assert len(dropped_arr) == args.reps
     assert len(blocked_arr) == args.reps
@@ -19,16 +24,23 @@ def analyse(args):
     var_blocked = np.sum((blocked_arr-mean_blocked)**2) / (len(blocked_arr)-1)
     
     with open(out_file, 'w') as f:
-        f.write('Displaying arrays\n')
+        f.write('------------------- Displaying arrays -------------------\n')
         f.write(f'Drop array: {dropped_arr}\n')
         f.write(f'Block array: {blocked_arr}\n')
-        f.write('Displaying statistics\n')
+        f.write('---------------------------------------------------------\n')
+        f.write('----------------- Displaying statistics -----------------\n')
+        f.write('|---------------- Drop Rate ----------------------------|\n')
         f.write(f'Mean drop rate: {mean_dropped}\n')
         f.write(f'Variance of drop rate: {var_dropped}\n')
         f.write(f'Standard deviation of drop rate: {np.sqrt(var_dropped)}\n')
+        f.write(f'Half-width of drop rate: {t_value*(np.sqrt(var_dropped)/np.sqrt(len(dropped_arr)))}\n')
+        f.write('---------------------------------------------------------\n')
+        f.write('|---------------- Block Rate ---------------------------|\n')
         f.write(f'Mean of block rate: {mean_blocked}\n')
         f.write(f'Variance of block rate: {var_blocked}\n')
         f.write(f'Standard deviation of block rate: {np.sqrt(var_blocked)}\n')
+        f.write(f'Half-width of block rate: {t_value*(np.sqrt(var_blocked)/np.sqrt(len(blocked_arr)))}\n')
+        f.write('---------------------------------------------------------\n')
 
     ######### Check correctness of vectorised op #########
     # test_var = 0
@@ -44,6 +56,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process simulation args')
     parser.add_argument('--reps', default=10, type=int, help='Number of repitions of simulation runs (n) in lectures')
     parser.add_argument('--num_reserve', default=0, type=int, help='Number of reserve channels')
+    parser.add_argument('--alpha', default=0.05, type=float, help='Level of significance alpha')
     args = parser.parse_args()
 
     analyse(args)
